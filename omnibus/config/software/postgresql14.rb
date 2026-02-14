e7d91c0b3c2bf83c6e31edb588ea060ec8e3ec1b
kaitokimuraofficial <k.kaito0422japan@google.com>
2026-02-14 22:18:19 +0900

    Modify copied file for PostgreSQL 14.20

--- omnibus/config/software/postgresql14.rb
+++ omnibus/config/software/postgresql14.rb
@@ -14,9 +14,9 @@
 # limitations under the License.
 #
 
-name "postgresql13"
+name "postgresql14"
 
-default_version "13.22"
+default_version "14.20"
 
 license "PostgreSQL"
 license_file "COPYRIGHT"
@@ -31,21 +31,9 @@ dependency "config_guess"
 
 # version_list: url=https://ftp.postgresql.org/pub/source/v#{version}/ filter=*.tar.bz2
 
-version("13.22") do
-  source url: "#{ENV['ARTIFACTORY_REPO_URL']}/postgresql13/#{name}-#{version}.tuxcare.1.0.0.tar.bz2",
-         authorization: "X-JFrog-Art-Api:#{ENV['ARTIFACTORY_TOKEN']}",
-         sha256: "d36d83dc89e625502cf6fb1d0529642ba1266bd614b4e4a41cefd1dddcf09080"
-end
-
-version("13.21") { source sha256: "dcda1294df45f033b0656cf7a8e4afbbc624c25e1b144aec79530f74d7ef4ab4" }
-version("13.18") { source sha256: "ceea92abee2a8c19408d278b68de6a78b6bd3dbb4fa2d653fa7ca745d666aab1" }
-version("13.14") { source sha256: "b8df078551898960bd500dc5d38a177e9905376df81fe7f2b660a1407fa6a5ed" }
-version("13.5") { source sha256: "9b81067a55edbaabc418aacef457dd8477642827499560b00615a6ea6c13f6b3" }
-version("13.6") { source sha256: "bafc7fa3d9d4da8fe71b84c63ba8bdfe8092935c30c0aa85c24b2c08508f67fc" }
+version("14.20") { source sha256: "7527f10f1640761bc280ad97d105d286d0cf72e54d36d78cf68e5e5f752b646b" }
 
 source url: "https://ftp.postgresql.org/pub/source/v#{version}/postgresql-#{version}.tar.bz2"
-internal_source url: "#{ENV["ARTIFACTORY_REPO_URL"]}/postgresql13/#{name}-#{version}.tuxcare.1.0.0.tar.bz2",
-                authorization: "X-JFrog-Art-Api:#{ENV["ARTIFACTORY_TOKEN"]}"
 
 relative_path "postgresql-#{version}"
 
